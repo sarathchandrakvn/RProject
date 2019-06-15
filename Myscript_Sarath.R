1646817613138051bb9161532d438afc172efa08
Sarath <sarath.x.kappagantula@kp.org>
2019-06-15 10:21:01 -0700

    Some r code

--- Myscript_Sarath.R
+++ Myscript_Sarath.R
@@ -28,3 +28,14 @@ install.packages("mosaic")
 library("mosaic")
 
 install.packages("ggplot2")
+
+library(ggplot2)
+
+table(doctorVisits$health)
+
+barplot(table(doctorVisits$health))
+
+
+ggplot(doctorVisits, aes(x = lfam, fill = sex)) + theme_bw() + geom_histogram() + facet_wrap(~idp+physlim+child) + labs(y = "Frequency", x = "Family Income",  title = "Histogram: Contacts With Medical Doctor")
+
+
