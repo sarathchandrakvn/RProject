45901f7517bd56f73a419e3aef1f1abc1145a5e8
Sarath <sarath.x.kappagantula@kp.org>
2019-06-14 20:53:50 -0700

    R scripts

--- Myscript_Sarath.R
+++ Myscript_Sarath.R
@@ -1,1 +1,26 @@
-help()
+
+setwd("/Users/sarath.chandra/Desktop/Study/Residency")
+
+doctorVisits <- read.csv("dataset_DoctorContacts.csv")
+
+summary (doctorVisits)
+
+View(doctorVisits)
+
+age <- doctorVisits[13]
+
+View(age)
+
+length(age)
+
+mdu <- doctorVisits[2]
+
+length(mdu)
+
+View(mdu)
+
+df <- data.frame(age,mdu)
+
+plot(df)
+
+
